e73c39c28c6e4b4ab6050e5a841a6da60ab1fd9d
Sebastien <sebastien@o-pa.fr>
2020-07-27 13:40:38 +0200

    vendor added to .gitignore and moved to / autoload classes remove /.htaccess

--- public/index.php
+++ public/index.php
@@ -2,17 +2,8 @@
 define('ROOT_DIR', __DIR__.'/..');
 define('TEMPLATES_DIR', ROOT_DIR.'/templates');
 
-require_once (ROOT_DIR.'/src/vendor/autoload.php');
-require_once (ROOT_DIR.'/src/Core/Router.php');
-require_once (ROOT_DIR.'/src/Core/Route.php');
-require_once (ROOT_DIR.'/src/Core/Controller.php');
-require_once (ROOT_DIR.'/src/Controller/PublicController.php');
-require_once (ROOT_DIR.'/src/Controller/AdminController.php');
-require_once(ROOT_DIR . '/src/Controller/Public/HomeController.php');
-require_once(ROOT_DIR . '/src/Controller/Public/BlogController.php');
-require_once(ROOT_DIR . '/src/Controller/Admin/HomeController.php');
-require_once(ROOT_DIR . '/src/Controller/Admin/BlogController.php');
-require_once(ROOT_DIR . '/src/Core/Page.php');
+require_once (ROOT_DIR.'/vendor/autoload.php');
+
 
 $router = new Core\Router();
 
