deb7a1835437945ee7511ef604e2433f2e71d431
Sebastien <sebastien@o-pa.fr>
2020-08-11 14:58:00 +0200

    Single Post page with Dynamic Data

--- src/Controller/PublicController/BlogController.php
+++ src/Controller/PublicController/BlogController.php
@@ -4,7 +4,9 @@
 namespace Controller\PublicController;
 
 use Core\Controller;
+use Models\CommentManager;
 use Models\PostManager;
+use Models\UserManager;
 
 
 class BlogController extends Controller
@@ -12,14 +14,19 @@ class BlogController extends Controller
 
     public function executeShow()
     {
-        $posts = (new PostManager())->getPosts(['dateCreated' => 'DESC'], ['count_row' => 10]);
-        if (!empty($posts)){
-            $this->templateVars['posts'] = $posts;
-        }
+        $this->templateVars['posts'] = (new PostManager())->getPosts(['dateCreated' => 'DESC'], ['count_row' => 10]);
         $this->render('@public/blog.html.twig');
     }
 
-    public function executeShowPost(){
+    public function executeShowPost()
+    {
+        $post = (new PostManager())->findOneBy('post', ['id' => $this->params['id']]);
+        $this->templateVars['post'] = $post;
+
+        $this->templateVars['author'] = (new UserManager())->findOneBy('user', ['id' => $post->getUserId()]);
+        $this->templateVars['comments'] = (new CommentManager())->findBy('comment', ['postId' => $this->params['id']]);
+        $this->templateVars['lastPosts'] = (new CommentManager())->findAll('post',[],['count_row' => 5]);
+
         $this->render('@public/single-post.html.twig');
     }
 }
