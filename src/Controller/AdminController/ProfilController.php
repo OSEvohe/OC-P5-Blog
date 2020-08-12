bc1b53814ce8f25adb48b7aa88a4dcba6398a2b6
Sebastien <sebastien@o-pa.fr>
2020-08-12 09:51:41 +0200

    Display admin profile page with dynamic data

--- src/Controller/AdminController/ProfilController.php
+++ src/Controller/AdminController/ProfilController.php
@@ -4,27 +4,33 @@
 namespace Controller\AdminController;
 
 use Core\Controller;
+use Models\ProfileManager;
+use Models\SocialNetworkManager;
 
 
 class ProfilController extends Controller
 {
     public function executeShow()
     {
+        $this->templateVars['profile'] = (new ProfileManager())->findAll('profile')[0];
         $this->render('@admin/profil.html.twig');
     }
 
     public function executeShowSocial()
     {
+        $this->templateVars['socialNetworks'] = (new SocialNetworkManager())->findAll('socialNetwork', ['name' => 'ASC']);
         $this->render('@admin/social.html.twig');
     }
 
     public function executeEditSocial()
     {
+        $this->templateVars['network'] = (new SocialNetworkManager())->findOneBy('socialNetwork', ['id' => $this->params['id']]);
         $this->render('@admin/social_edit.html.twig');
     }
 
     public function executeDeleteSocial()
     {
+        $this->templateVars['network'] = (new SocialNetworkManager())->findOneBy('socialNetwork', ['id' => $this->params['id']]);
         $this->render('@admin/social_delete.html.twig');
     }
 }
