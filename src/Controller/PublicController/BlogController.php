1584116b5089a99b80c292d9ebb36036a700d871
Sebastien <sebastien@o-pa.fr>
2020-08-25 12:07:06 +0200

    Process new comment form (frontend) issue : #18

--- src/Controller/PublicController/BlogController.php
+++ src/Controller/PublicController/BlogController.php
@@ -4,6 +4,7 @@
 namespace Controller\PublicController;
 
 use Core\Controller;
+use Entity\Comment;
 use Models\CommentManager;
 use Models\PostManager;
 use Models\UserManager;
@@ -25,13 +26,34 @@ class BlogController extends Controller
     public function executeShowPost()
     {
         $post = (new PostManager())->findOneBy(['id' => $this->params['id']]);
-        $this->templateVars['post'] = $post;
 
+        if ($this->isFormSubmit('comment_newSubmit')) {
+            $this->templateVars['errors'] = $this->processNewCommentForm();
+        }
+
+        $this->templateVars['post'] = $post;
         $this->templateVars['author'] = (new UserManager())->findOneBy(['id' => $post->getUserId()]);
-        $this->templateVars['comments'] = (new CommentManager())->findBy(['postId' => $this->params['id']]);
+        $this->templateVars['comments'] = (new CommentManager())->findBy(['postId' => $this->params['id'], 'visible' => 1]);
         $this->templateVars['lastPosts'] = (new CommentManager())->findAll([], ['count_row' => 5]);
 
         $this->getSocialNetworks();
         $this->render('@public/single-post.html.twig');
+    }
+
+    /**
+     * Process the new comment form and redirect to the single post page if a new comment is created
+     * @return array Return errors as an array if any
+     */
+    private function processNewCommentForm()
+    {
+        $comment = new Comment($_POST);
+        $comment->hydrate(['userId' => 1, 'postId' => $this->params['id'], 'visible' => 0]); //TODO get userId from logged user
+
+        if ($comment->isValid()) {
+            (new CommentManager())->create($comment);
+            $this->redirect('/blog/' . $this->params['id']);
+        }
+
+        return $comment->getConstraintsErrors();
     }
 }
