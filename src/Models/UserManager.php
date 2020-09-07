f377768b31151d47da8ded53981b079a96f293bb
Sebastien <sebastien@o-pa.fr>
2020-09-07 23:56:44 +0200

    Add controller UserController #15 #16 #17 Update UserManager, admin (id : 1) cannot be deleted

--- src/Models/UserManager.php
+++ src/Models/UserManager.php
@@ -4,24 +4,40 @@
 namespace Models;
 
 
+use Core\Entity;
+
 class UserManager extends \Core\Manager
 {
 
 
     /**
      * Return a list of users having $role role
-     * @param string $role
+     * $role may be a string or an array
+     * @param array|string $role
      * @return array
      */
-    public function findByRole(string $role){
+    public function findByRole($role)
+    {
         $usersWithRole = [];
         $users = $this->findAll();
-        foreach ($users as $user){
-            if (array_search($role,$user->getRole()) !== FALSE){
-                $usersWithRole[] = $user;
+        foreach ($users as $user) {
+            foreach ($user->getRole() as $userRole) {
+                if (in_array($userRole, $role)) {
+                    $usersWithRole[] = $user;
+                    break;
+                }
             }
         }
         return $usersWithRole;
+    }
+
+    public function delete(Entity $user)
+    {
+        /* Cannot delete Admin #1 account */
+        if ($user->getId() != 1) {
+            parent::delete($user);
+        }
+
     }
 
 }
